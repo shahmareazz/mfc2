7c1f747989de538c169c67f2cfd22434dae1e369
shahmareazz <136588415+shahmareazz@users.noreply.github.com>
2023-07-12 23:45:12 +0530

    completed project

---
+++ src/components/Aboutus.jsx
@@ -0,0 +1,27 @@
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import { useState, useEffect } from 'react';
+import { Col, Row } from 'react-bootstrap';
+function Aboutus() {
+    return (
+        <Container className="pt-2">
+            <h5 className="my-5 text-center display-4">About Us</h5>
+            <Row>
+                <Col md={12}>
+                    we are passionate about creating a memorable dining experience for our valued guests.
+                     We take pride in offering a diverse menu that caters to all tastes and preferences, 
+                     whether you're a meat lover, seafood enthusiast, or a vegetarian.
+                    Our culinary team is dedicated to crafting dishes that showcase the finest ingredients, 
+                    carefully selected to ensure freshness and quality. From succulent chicken delicacies to tender 
+                    and juicy meats, our menu offers a wide range of options to satisfy your cravings.
+                    For those who savor the flavors of the sea, our selection of fish dishes is a true delight.
+                    From grilled salmon to delectable seafood curries, we strive to bring you the finest and freshest catches of the day.
+                    We also understand the importance of catering to our vegetarian guests. Our dedicated vegetarian section features a variety
+                     of flavorful and wholesome dishes that celebrate the richness of vegetables and plant-based ingredients.
+                    From aromatic vegetable biryanis to flavorful tofu stir-fries, we ensure that every vegetarian palate is delighted.
+                </Col>
+            </Row>
+        </Container>
+    );
+}
+export default Aboutus;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './App.css';
+import Home from './components/Home';
+import Aboutus from './components/Aboutus';
+import Header from './components/Header';
+import { BrowserRouter, Route, Routes, Switch} from 'react-router-dom';
+import { Contactus } from './components/Contactus';
+import Register from './components/Register';
+import Login from './components/Login';
+import Footer from './components/Footer';
+import Menu from './components/Menu';
+import VegMenu from './components/VegMenu';
+import FishMenu from './components/FishMenu';
+import ChickenMenu from './components/ChickenMenu';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const myStyle={
+    backgroundImage:"url('/images/bg.jpg')",
+    minHeight:'500px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+};
+  return (
+    <>
+    <Header />
+    <section style={myStyle}>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/Menu" element={<Menu />}></Route>
+      <Route path="/Aboutus" element={<Aboutus />}></Route>
+      <Route path="/Contactus" element={<Contactus />}></Route>
+      <Route path="/Register" element={<Register />}></Route>
+      <Route path="/Login" element={<Login />}></Route>
+      <Route path="/VegMenu" element={<VegMenu />}></Route>
+      <Route path="/FishMenu" element={<FishMenu />}></Route>
+      <Route path="/ChickenMenu" element={<ChickenMenu />}></Route>
+    </Routes>
+    </section>
+  <Footer />
+  </>
+  );
+}
+
+export default App;
